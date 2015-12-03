7806ba67c1a57e250fa06e23e72ea8480cc1e235
Marcin Milewicz <inspiration075@gmail.com>
2015-12-03 18:52:16 +0100

    RecognizeRestController has been finished

--- src/main/java/pl/androidland/controllers/RecognizeRestController.java
+++ src/main/java/pl/androidland/controllers/RecognizeRestController.java
@@ -3,16 +3,22 @@ package pl.androidland.controllers;
 
 import com.bitsinharmony.recognito.MatchResult;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import pl.androidland.io.VoiceUploader;
 import pl.androidland.recognition.RecognitionService;
+import pl.androidland.responses.RegisterResponse;
 import pl.androidland.responses.Response;
+import pl.androidland.responses.ResponseType;
+import pl.androidland.responses.SpeakerInfo;
 
 import javax.sound.sampled.UnsupportedAudioFileException;
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/recognize")
@@ -26,14 +32,72 @@ public class RecognizeRestController {
     @ResponseBody
     ResponseEntity<Response> recognize(@RequestParam("voice") MultipartFile file) throws IOException, UnsupportedAudioFileException {
         VoiceUploader uploader = new VoiceUploader((file));
-        if (!uploader.isUploaded()) {
-            return "You failed to upload  => ";
-        }
+        if (!uploader.isUploaded())
+            return getFailUploadResponse();
 
         String pathname = uploader.getFilePath();
+        List<MatchResult<String>> result = recognitionService.recognize(new File(pathname));
 
-        MatchResult result = recognitionService.recognize(new File(pathname));
+        if (result.isEmpty())
+            return getFailRecognitionResponse();
 
-        return "I think it is " + result.getKey() + " with " + result.getLikelihoodRatio() + "% confidence.";
+        MatchResult<String> bestResult = result.get(0);
+        SpeakerInfo speakerInfo = createSpeakerInfo(bestResult);
+
+        return getSpeakerInfoResponse(speakerInfo);
     }
+
+    @RequestMapping(value = "/all", method = RequestMethod.POST)
+    public
+    @ResponseBody
+    ResponseEntity<Response> recognizeAll(@RequestParam("voice") MultipartFile file) throws IOException, UnsupportedAudioFileException {
+        VoiceUploader uploader = new VoiceUploader((file));
+        if (!uploader.isUploaded())
+            return getFailUploadResponse();
+
+        String pathname = uploader.getFilePath();
+        List<MatchResult<String>> results = recognitionService.recognize(new File(pathname));
+
+        if (results.isEmpty())
+            return getFailRecognitionResponse();
+
+        List<SpeakerInfo> speakersInfo = results
+                .stream()
+                .map(this::createSpeakerInfo)
+                .collect(Collectors.toList());
+
+        return getSpeakerInfoResponse(speakersInfo);
+    }
+
+    private <T> ResponseEntity<Response> getSpeakerInfoResponse(T speakerInfo) {
+        return new ResponseEntity<>(RegisterResponse.Builder.
+                newResponse(ResponseType.SUCCESSFUL)
+                .withMessage("Results of voice recognition have been found")
+                .withContent(speakerInfo)
+                .build(), HttpStatus.OK);
+    }
+
+    private SpeakerInfo createSpeakerInfo(MatchResult<String> result) {
+        return SpeakerInfo.Builder.
+                newInfo()
+                .withName(result.getKey())
+                .withLikehoodRatio(result.getLikelihoodRatio())
+                .build();
+    }
+
+    private ResponseEntity<Response> getFailUploadResponse() {
+        return new ResponseEntity<>(RegisterResponse.Builder.
+                newResponse(ResponseType.FAIL)
+                .withMessage("File has been not upload")
+                .build(), HttpStatus.OK);
+    }
+
+    private ResponseEntity<Response> getFailRecognitionResponse() {
+        return new ResponseEntity<>(RegisterResponse.Builder.
+                newResponse(ResponseType.FAIL)
+                .withMessage("There is no chance to recognize this voice")
+                .build(), HttpStatus.OK);
+    }
+
+
 }
